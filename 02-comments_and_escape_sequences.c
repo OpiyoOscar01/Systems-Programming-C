0f40bc011e97489bbde5cfe724c6c83f04418b93
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-01-12 12:57:58 +0300

    Covered comments and escpae sequnces in C

---
+++ 02-comments_and_escape_sequences.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+  //This is a comment
+  printf("Hello, World!\n");
+  /*
+  This is
+  a multiline comment
+  */
+ printf("My name is\nJohn Doe");
+ /*
+ Escape characters are used to format the output of a string.
+  \n - New line
+  \t - Tab
+  \b - Backspace
+  \r - Carriage return
+  \f - Form feed
+  \v - Vertical tab
+  \a - Alert
+  \0 - Null
+  \\ - Backslash
+  \' - Single quote
+  \" - Double quote
+  \? - Question mark
+  \xhh - Hexadecimal number
+  \ooo - Octal number
+  \ddd - Decimal number
+ */
+printf("I love programming\n");
+printf("What is your favorite programming language\?\n");
+printf("\"Clean Code\" is a great book\n");
+  return 0;
+}
